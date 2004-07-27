6d8ff291721c94fce1eb1f203ede742b38e3b85e
Kouhei Sutou <kou@cozmixng.org>
2004-07-27 03:49:37 +0000

    * added HTTP header parser.

---
+++ lib/xsm/xml-rpc/http.scm
@@ -0,0 +1,119 @@
+(define-module xsm.xml-rpc.http
+  (use srfi-13)
+  (use gauche.version)
+  (use text.tr)
+  (use util.list))
+(select-module xsm.xml-rpc.http)
+
+(define-class <xml-rpc-http-error> (<exception>)
+  ())
+
+(define-class <not-supported-http-version> (<xml-rpc-http-error>)
+  (version :accessor version-of :init-keyword :varsion))
+
+(define-class <http-error> (<xml-rpc-http-error>)
+  ((code :accessor code-of :init-keyword :code)
+   (phrase :accessor phrase-of :init-keyword :phrase)))
+
+(define-class <invalid-content-type> (<xml-rpc-http-error>)
+  ((content-type :accessor content-type :init-keyword :content-type)))
+
+(define-class <invalid-content-length> (<xml-rpc-http-error>)
+  ((content-length :accessor content-length :init-keyword :content-length)))
+
+(define (read-with-timeout input reader timeout not-response-handler)
+  (if (char-ready? input)
+    (reader input)
+    (let ((result #f)
+          (selector (make <selector>)))
+      (selector-add! selector
+                     input
+                     (lambda (in . args)
+                       (selector-delete! selector input #f #f)
+                       (set! result (reader in)))
+                     '(r))
+      (let ((retry #f))
+        (if (zero? (begin
+                     (call/cc
+                      (lambda (cont)
+                        (set! retry cont)))
+                     (selector-select selector timeout)))
+          (not-response-handler retry)
+          result)))))
+
+(define (read-required-block input size eof-handler)
+  (define (more-read size)
+    (define retry-count 3)
+    (read-with-timeout input (make-reader size) (list 3 0)
+                       (lambda (retry)
+                         (dec! retry-count)
+                         (if (< retry-count 0)
+                           (error "not response")
+                           (retry)))))
+  
+  (define (make-reader size)
+    (lambda (in . args)
+      (read-block size in)))
+
+  (define (read-more-if-need block)
+    (debug (list "read body" block))
+    (cond ((eof-object? block) (eof-handler))
+          ((< (string-size block) size)
+           (debug (list "more reading..." size (string-size block)
+                        (- size (string-size block))))
+           (read-more-if-need
+            (string-append block
+                           (more-read (- size (string-size block))))))
+          (else
+           (debug (list "got block" block))
+           block)))
+    
+  (read-more-if-need (more-read size)))
+
+(define (http-header-read input)
+  (define counter 3)
+  (define (next-line)
+    (read-with-timeout input read-line (list 10 0)
+                       (lambda (retry)
+                         (dec! counter)
+                         (if (> counter 0)
+                           (retry)
+                           (error "not response")))))
+
+  (let ((status-line (next-line)))
+    (rxmatch-let (rxmatch #/HTTP\/(\d+\.\d+) (\d+) (.+)$/ status-line)
+        (#f version code phrase)
+      (unless (and (version<=? "1.0" version)
+                   (version<=? version "1.1"))
+        (raise (make <not-supported-http-version> :version version)))
+      (unless (= 200 (string->number code))
+        (raise (make <http-error>
+                 :code (string->number code)
+                 :phrase phrase)))))
+  
+  (let loop ((result '())
+             (line (next-line)))
+    (cond ((or (eof-object? line)
+               (string-null? line))
+           result)
+          (else
+           (loop (cons (let ((key&value (string-split line #/\s*:\s*/)))
+                         (cons (string-tr (string-upcase (car key&value))
+                                          "\-" "_")
+                               (string-join (cdr key&value)
+                                            ":")))
+                       result)
+                 (next-line))))))
+
+(define (http-header-parse input)
+  (let* ((alist (http-header-read input))
+         (content-type (assoc-ref alist "CONTENT_TYPE"))
+         (content-length (assoc-ref alist "CONTENT_LENGTH")))
+    (unless (equal? "text/xml" content-type)
+      (raise (make <invalid-content-type> :content-type content-type)))
+    (unless (and content-length (string->number content-length))
+      (raise (make <invalid-content-length> :content-length content-length)))
+
+    (string->number content-length)))
+    
+(provide "xsm/xml-rpc/http")
--- test/test-xml-rpc-http.scm
+++ test/test-xml-rpc-http.scm
@@ -1,14 +1,85 @@
 #!/usr/bin/env gosh
 
 (define-module xml-rpc-http-test
-  (use srfi-1)
-  (use srfi-19)
-  (use rfc.base64)
   (use test.unit)
-  (use sxml.tools)
-  (use text.tree)
   (extend xsm.xml-rpc.http))
 (select-module xml-rpc-http-test)
 
+(define (%http-header-read str)
+  (http-header-read (open-input-string str)))
+
+(define (%http-header-parse str)
+  (http-header-parse (open-input-string str)))
+
+(define-assertion (assert-http-error code phrase str)
+  (define (%http-header-read str)
+    ((with-module xsm.xml-rpc.http http-header-read)
+     (open-input-string str)))
+  
+  (assert-raise (with-module xsm.xml-rpc.http <http-error>)
+                (lambda ()
+                  (%http-header-read str)))
+
+  (with-exception-handler
+      (lambda (e)
+        (and (equal? code (ref e 'code))
+             (equal? phrase (ref e 'phrase))))
+    (lambda ()
+      (%http-header-read "HTTP/1.1 500 Internal Server Error"))))
+
 (define-test-case "xml-rpc.http test"
-  )
+  ("http-header-read test"
+   (assert-equal '()
+                 (%http-header-read "HTTP/1.1 200 OK"))
+   (assert-raise <not-supported-http-version>
+                 (lambda () (%http-header-read "HTTP/0.1 200 OK")))
+   (assert-http-error 500 "Internal Server Eror"
+                      "HTTP/1.1 500 Internal Server Error")
+
+   (assert-lset-equal
+    '(("CONNECTION" . "close")
+      ("CONTENT_LENGTH" . "158")
+      ("CONTENT_TYPE" . "text/xml")
+      ("DATE" . "Fri, 17 Jul 1998 19:55:08 GMT")
+      ("SERVER" . "UserLand Frontier/5.1.2-WinNT"))
+    (%http-header-read
+     (string-join (list "HTTP/1.1 200 OK"
+                        "Connection: close"
+                        "Content-Length: 158"
+                        "Content-Type: text/xml"
+                        "Date: Fri, 17 Jul 1998 19:55:08 GMT"
+                        "Server: UserLand Frontier/5.1.2-WinNT")
+                  "\n"))))
+  
+  ("http-header-parse test"
+   (assert-equal 159
+                 (%http-header-parse
+                  (string-join (list "HTTP/1.1 200 OK"
+                                     "Connection: close"
+                                     "Content-Length: 159"
+                                     "Content-Type: text/xml"
+                                     "Date: Fri, 17 Jul 1998 19:55:08 GMT"
+                                     "Server: UserLand Frontier/5.1.2-WinNT")
+                               "\n")))
+   (assert-raise
+    <invalid-content-type>
+    (lambda ()
+      (%http-header-parse
+       (string-join (list "HTTP/1.1 200 OK"
+                        "Connection: close"
+                        "Content-Length: 158"
+                        "Content-Type: text/xml; charset=UTF-8"
+                        "Date: Fri, 17 Jul 1998 19:55:08 GMT"
+                        "Server: UserLand Frontier/5.1.2-WinNT")
+                    "\n"))))
+   (assert-raise
+    <invalid-content-length>
+    (lambda ()
+      (%http-header-parse
+       (string-join (list "HTTP/1.1 200 OK"
+                        "Connection: close"
+                        "Content-Length: aaa"
+                        "Content-Type: text/xml"
+                        "Date: Fri, 17 Jul 1998 19:55:08 GMT"
+                        "Server: UserLand Frontier/5.1.2-WinNT")
+                    "\n"))))))
