0fbb07a16ff53962df4e32b9ea96a5ace86db103
Kouhei Sutou <kou@cozmixng.org>
2004-07-27 16:58:23 +0000

    * passed tests.

--- test/test-xml-rpc-client.scm
+++ test/test-xml-rpc-client.scm
@@ -9,12 +9,12 @@
 (define-test-case "XML-RPC client test"
   ("make-request test"
    (assert-equal `(*TOP*
-                   (*PI* xml ,#`"version='1.0' encoding=',(gauche-character-encoding)'")
+                   (*PI* xml ,#`"version=\"1.0\" encoding=\",(gauche-character-encoding)\"")
                    (methodCall
                     (methodName "examples.getStateName")
                     (params
-                     (param (value (int 41)))
-                     (param (value (double -41.41))))))
+                     (param (value (int "41")))
+                     (param (value (double "-41.41"))))))
                  (ssax:xml->sxml
                   (open-input-string
                    (make-request "examples.getStateName" 41 -41.41))
